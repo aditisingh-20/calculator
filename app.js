1b045e4b87df86018a18c5e1abf704d043c28eca
Aditi Singh <73885110+aditisingh-20@users.noreply.github.com>
2023-06-28 16:31:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const input=document.querySelector('#screen');
+var str="";
+
+function button(id){
+    const btn=document.querySelector(`#${id}`);
+    input.placeholder+=btn.value;
+    str+=btn.value;
+}
+function clearpl(){
+    if(input.placeholder!==""){
+        if(input.placeholder!=="INVALID"){
+            const len=input.placeholder.length;
+            input.placeholder=input.placeholder.substring(0,len-1);
+            str=str.substring(0,len-1);
+        }
+        else{
+            input.placeholder="";
+            str="";
+        }
+    }
+}
+function clearall(){
+    input.placeholder="";
+    str="";
+}
+function invalid(){
+    input.placeholder="INVALID";
+    str="";
+}
+function calculate(){
+    if(str.length<3)
+    {
+        invalid();
+    }
+    if(parseInt(str.charAt(0))==='NaN')
+    {
+        invalid();
+    }
+    if(str.length%2===0)
+    {
+        invalid();
+    }
+    const res=eval(str);
+    input.placeholder=res;
+}
